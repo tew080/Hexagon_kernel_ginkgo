5fa27f097199e606081be091054fc25c663cc201
Sami Tolvanen <samitolvanen@google.com>
2019-10-31 12:57:05 -0700

    arm64: lse: fix LSE atomics with LLVM's integrated assembler
    commit e0d5896bd356cd577f9710a02d7a474cdf58426b upstream.
    
    Unlike gcc, clang considers each inline assembly block to be independent
    and therefore, when using the integrated assembler for inline assembly,
    any preambles that enable features must be repeated in each block.
    
    This change defines __LSE_PREAMBLE and adds it to each inline assembly
    block that has LSE instructions, which allows them to be compiled also
    with clang's assembler.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/671
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Tested-by: Andrew Murray <andrew.murray@arm.com>
    Tested-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Andrew Murray <andrew.murray@arm.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    [nd: backport adjusted due to missing:
      commit addfc38672c7 ("arm64: atomics: avoid out-of-line ll/sc atomics")]
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I3829aee041ceb39c4252df262be93dde0ca7c156
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -6,6 +6,9 @@
 
 #include <linux/compiler_types.h>
 #include <linux/export.h>
+
+#define __LSE_PREAMBLE	".arch armv8-a+lse\n"
+
 #include <linux/stringify.h>
 #include <asm/alternative.h>
 #include <asm/cpucaps.h>
@@ -20,8 +23,6 @@
 
 #else	/* __ASSEMBLER__ */
 
-__asm__(".arch_extension	lse");
-
 /* Move the ll/sc atomics out-of-line */
 #define __LL_SC_INLINE		notrace
 #define __LL_SC_PREFIX(x)	__ll_sc_##x
@@ -33,7 +34,7 @@ __asm__(".arch_extension	lse");
 
 /* In-line patching at runtime */
 #define ARM64_LSE_ATOMIC_INSN(llsc, lse)				\
-	ALTERNATIVE(llsc, lse, ARM64_HAS_LSE_ATOMICS)
+	ALTERNATIVE(llsc, __LSE_PREAMBLE lse, ARM64_HAS_LSE_ATOMICS)
 
 #endif	/* __ASSEMBLER__ */
 #else	/* CONFIG_AS_LSE && CONFIG_ARM64_LSE_ATOMICS */
